736753c2cf7c3d6f8cebfe803c00f50f5a464c09
codyjhill99 <codyjhill99@gmail.com>
2018-01-18 21:58:42 -0500

    Created a word in progress center jumpotron, add dynamic background image based on location

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var rain = "https://caminowordpress-4df7.kxcdn.com/wp-content/uploads/rain1.jpg";
+var clearSky = "http://ak8.picdn.net/shutterstock/videos/723328/thumb/1.jpg";
 
 navigator.geolocation.getCurrentPosition(success);
 
@@ -22,10 +24,20 @@ function success(pos) {
         document.getElementById("location").innerHTML = response.name + "," + response.sys.country;
         document.getElementById("temperature").innerHTML = response.main.temp * 9 / 5 + 32;
         document.getElementById("weather").innerHTML = response.weather[0].description;
+            
+        //changes the background based on the weather api resonse 
+        var body = document.getElementsByTagName('body')[0];
+        if(response.weather[0].main === "Clear") {
+            body.style.backgroundImage = 'url('+ clearSky + ')';
+        } else if (response.weather[0].main === "Rain") {
+            body.style.backgroundImage = 'url('+ rain + ')';
+        }
+            
+            
         }
     }
     
 }
 
-
-
+//rain https://caminowordpress-4df7.kxcdn.com/wp-content/uploads/rain1.jpg
+//clear sky http://ak8.picdn.net/shutterstock/videos/723328/thumb/1.jpg
